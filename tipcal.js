7b31b496894de593a41fa8a9fcd607170b984db2
Langershivam <shivamlanger2001@gmail.com>
2023-05-19 14:41:54 +0530

    tipcal

--- tipcal.js
+++ tipcal.js
@@ -1,23 +1,32 @@
-const tip=document.getElementById("range");
-const tipp=document.querySelector(".ran");
-const tipamt=document.querySelector(".tipamt");
-const totalbill=document.querySelector(".bill");
-let tipc="";
-tip.addEventListener("change",function(e){
-    let input=document.getElementById("num").value;
-    let amount = parseInt(input);
-    let tamt=parseInt(e.target.value);
-    if(input.length == 0){
-        alert("Amount Required");
-    }else{
-        tipp.textContent=`${tamt}%`;
-        totalbill.textContent=tipCal(amount,tamt);
-        tipamt.textContent=`${tipc}`;
-    }
+const tip = document.getElementById("range");
+const tipp = document.querySelector(".ran");
+const tipamt = document.querySelector(".tipamt");
+const totalbill = document.querySelector(".bill");
+let input = document.getElementById("num");
+let tipc = "";
+function calculate() {
     
-    function tipCal(amt,tip){
-         tipc=(amt*tip)/100;
-
-        return tipc+amt;
+    let amount = parseInt(input.value);
+    if(!isNaN(amount)){
+        let tamt = parseInt(document.getElementById("range").value);
+        if (amount == 0) {
+            alert("Amount Required");
+        } else {
+            tipp.textContent = `${tamt}%`;
+            totalbill.textContent = tipCal(amount, tamt);
+            tipamt.textContent = `${tipc}`;
+        }
+    
+        function tipCal(amt, tip) {
+            tipc = (amt * tip) / 100;
+    
+            return tipc + amt;
+        }
+    }else{
+        totalbill.textContent = 0;
+            tipamt.textContent = 0;
     }
-});
+   
+}
+tip.addEventListener("change", calculate);
+input.addEventListener("keyup",calculate)
